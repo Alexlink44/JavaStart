577f3f51abe44b5f00ce5d467efafc9492cb4c17
Alexlink44 <94551312+Alexlink44@users.noreply.github.com>
2022-06-13 21:45:36 +0500

    Upload_01

---
+++ Task01.java
@@ -0,0 +1,15 @@
+public class Task01 {
+    public static void task() throws Exception {
+        int[] data = WorkingWithFiles.Input();
+        // int i = 0;
+        // while(i < data.length){
+        //     System.out.printf("- %s -\n", data[i]);
+        //     i++;
+        // }
+        double number = 0;
+        boolean flag = true;
+        if (data[1] == 0) {flag = false;}
+        else number = Math.pow(data[1], data[0]);
+        WorkingWithFiles.Output(number, flag);
+    }
+}
