926abf9ce65b80b256aa0acf2a07b6f90a1cbc95
Alex_T <alex.link_mail@e1.ru>
2022-06-20 19:13:41 +0500

    Temp

--- program.java
+++
@@ -1,8 +0,0 @@
-
-public class Program {
-    public static void main(String[] args) throws Exception {
-        int triangleTNum = Task02.InputData();
-        int triangleNumber = Task02.TriangularNumberCalculation(triangleTNum);
-        System.out.println(triangleNumber);
-    }
-}
