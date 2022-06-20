b184d410c919fa3cdea7ad3bfde8c2c575fff43d
Alex_T <alex.link_mail@e1.ru>
2022-06-20 19:14:33 +0500

    add

---
+++ Program.java
@@ -0,0 +1,8 @@
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        int triangleTNum = Task02.InputData();
+        int triangleNumber = Task02.TriangularNumberCalculation(triangleTNum);
+        System.out.println(triangleNumber);
+    }
+}
