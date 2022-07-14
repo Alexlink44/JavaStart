fd4def9c4e2a3fc2e40156f0fddbcb2bfc6f6822
Alexlink44 <94551312+Alexlink44@users.noreply.github.com>
2022-07-14 21:31:58 +0500

    Commit_2

--- ChessField.java
+++ ChessField.java
@@ -1,10 +1,68 @@
 public class ChessField {
-    public static int[][] Field(int[][] field, int[] turn, int position){
-        
+    public static int[][] Field(int[][] field, int[] turn, int position, int n) {
+       while (position < n) {
+            boolean flag = false;
+            while (!flag) {
+                int i = 0;
+                int j = 0;
+                while(field[i][j] != position){
+                    j = 0;
+                    while(j < turn.length - 1 & field[i][j] != position ){
+                        if(field[i][j] == position) break;
+                        j++;
+                    }
+                    if(field[i][j] == position) break;
+                    i++;
+                }
+
+                int indexI = 0;
+                int indexJ = 0;
+                while (turn[position] < 8 & field[indexI][indexJ] != 0) {
+                    while (turn[position] < 8 & indexI == -1 | indexJ == -1) {
+                        indexI = ChessField.Offset(i, turn[position], n, 2);
+                        indexJ = ChessField.Offset(i, turn[position], n, 0);
+                        turn[position]++;
+                    }
+                }
+                if (turn[position] == 8) {
+                    field[i][j] = 0;
+                    turn[position] = 0;
+                    position--;
+                } else {
+                    position = position + 1;
+                    field[indexI][indexJ] = position;
+                    flag = true;
+                }
+            }
+        }
+
         return field;
+
     }
 
-    
-
+    public static int Offset(int index, int turn, int n, int p) {
+        turn += p;
+        int result = 0;
+        int k = -1;
+        int h = 2;
+        int j = -1;
+        int b = 0;
+        int i = 0;
+        while (i < turn + 1) {
+            result = index + k;
+            k += h;
+            h += j;
+            i++;
+            b++;
+            if (b == 4) {
+                b = 0;
+                j = j * (-1);
+            }
+        }
+        if (index < 0 | index > n) {
+            index = -1;
+        }
+        return result;
+    }
 
 }
--- Program.java
+++ Program.java
@@ -1,13 +1,27 @@
 public class Program {
     public static void main(String[] args) throws Exception {
         int n = 5;
-        int position = 0;
+        int position = 1;
         int[] turn = new int [n];
         int[][] field = new int[n][n];
 
-        
-        
+        field[4][4] = 1;
 
+        // for (int i = 0; i < 8; i++) {
+        //     int m = ChessField.Offset(5, i, n, 0);
+        //     int l = ChessField.Offset(5, i, n, 2);
+        //     System.out.printf("i=%d, j=%d",m, l);
+        //     System.out.println();
+        // }
+        
+        field = ChessField.Field(field, turn, position, n);
+
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field.length; j++) {
+                System.out.printf("%d ", field[i][j]);
+            }
+            System.out.println();
+        }
 
     }
 }
